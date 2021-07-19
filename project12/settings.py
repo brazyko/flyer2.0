bc5904686e2efe6ff3814394115823c0dbcadb65
brazyko <odintsov.bogdan@gmail.com>
2021-07-19 11:58:25 +0300

    d

--- project12/settings.py
+++ project12/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -155,3 +156,5 @@ EMAIL_HOST_PASSWORD = ''
 
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
+
+django_heroku.settings(locals())

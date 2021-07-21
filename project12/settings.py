7a127397500b918bac17e3f2f22de47cc7a7a84a
brazyko <40731578+brazyko@users.noreply.github.com>
2021-07-21 21:04:27 +0300

    Update settings.py

--- project12/settings.py
+++ project12/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9r9j)5u5fli0z1suatvx)a!4kz^@@3@zv0!=@%k=&#a#zjxtxl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['wheellove.herokuapp.com','127.0.0.1:8000']
 
